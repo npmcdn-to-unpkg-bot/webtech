c22e5e535aa288474ac0cf3d35a0087981bae6cd
Michael Stypa <michael@stypa.info>
2015-11-27 00:38:24 +0100

    rename item db + SPA split

--- _attachments/src/verleihfix/verleihfix.ts
+++ _attachments/src/verleihfix/verleihfix.ts
@@ -1,41 +1,23 @@
 import {
   bootstrap,
   Component,
-  CORE_DIRECTIVES,
+  View,
   enableDevMode
 } from 'angular2/angular2';
 import {
-  Http,
-  HTTP_PROVIDERS
-} from 'angular2/http';
+  Grid
+} from './grid';
 
 @Component({
 selector: 'verleihfix',
-bindings: [CORE_DIRECTIVES,HTTP_PROVIDERS],
 templateUrl: 'verleihfix.html',
+directives: [Grid],
 styleUrls: ['style/verleihfix.css']
 })
-export class AppComponent {
-  items: any;
-  http; any;
-
-  constructor(http: Http) {
-    this.http = http;
-
-    this.http.get('http://localhost:15984/item/_all_docs?include_docs=true')
-      .map(res => res.json().rows.map(res => res.doc))
-      .subscribe(res => this.items = res);
-  }
-
-  rent() {
-    var selected = this.items.filter((item) => item.selected);
-    console.log(selected);
-  }
-
-  toggleSelected(item) {
-    item.selected = !item.selected;
+export class Verleihfix {
+  constructor() {
   }
 }
 
 enableDevMode();
-bootstrap(AppComponent).catch(err => console.error(err));;
+bootstrap(Verleihfix).catch(err => console.error(err));;
--- pushitems.sh
+++ pushitems.sh
@@ -1,7 +1,7 @@
-curl -u root:J6HoMqKo -X PUT "http://localhost:15984/item"
+curl -u root:J6HoMqKo -X PUT "http://localhost:15984/items"
 for i in $(find _attachments/items/*);
 do
   UUID=$(curl "http://localhost:15984/_uuids?count=1" | awk -F '"' '{print $4}')
-  curl -u root:J6HoMqKo -X PUT -d @$i "http://localhost:15984/item/$UUID"
+  curl -u root:J6HoMqKo -X PUT -d @$i "http://localhost:15984/items/$UUID"
 done;
 
