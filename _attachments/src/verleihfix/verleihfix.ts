94595839fd334d6169b69c43110c340cefff5a95
Michael Stypa <mstypa@uni-osnabrueck.de>
2015-12-17 16:17:57 +0100

    changed navigation and routes

--- _attachments/src/verleihfix/verleihfix.ts
+++ _attachments/src/verleihfix/verleihfix.ts
@@ -5,7 +5,7 @@ import {
   provide,
   enableDevMode
 } from 'angular2/angular2';
-import { 
+import {
   ROUTER_PROVIDERS,
   ROUTER_DIRECTIVES,
   RouterOutlet,
@@ -15,33 +15,26 @@ import {
   HashLocationStrategy
 } from 'angular2/router';
 import { Grid } from './grid';
-import { Start } from './start';
-import { Test } from './test';
-import { Login } from './login';
+import { Lendings } from './lendings';
+import { Navigation } from './navigation';
 
 @RouteConfig([
-  {path: '/', as: 'Start', component: Start },
-  {path: '/test', as: 'Test', component: Test },
+  {path: '/', redirectTo: '/grid' },
   {path: '/grid', as: 'Grid', component: Grid },
-  {path: '/login', as: 'Login', component: Login }
+  {path: '/lendings', as: 'Lendings', component: Lendings }
 ])
 @Component({
-selector: 'verleihfix'
-//directives: 'ROUTE_DIRECTIVES'
-})
-@View({
-directives: [RouterOutlet, RouterLink, ROUTER_DIRECTIVES],
+selector: 'verleihfix',
 templateUrl: 'verleihfix.html',
-styleUrls: ['style/verleihfix.css']
+styleUrls: ['style/verleihfix.css'],
+directives: [RouterOutlet, RouterLink, ROUTER_DIRECTIVES, Navigation]
 })
 export class Verleihfix {
-  //navItem any;
   constructor() {
   }
-  //toggleActive() {}
 }
 
 enableDevMode();
 bootstrap(Verleihfix, [ROUTER_PROVIDERS,
   provide(LocationStrategy, {useClass:HashLocationStrategy})]
-  ).catch(err => console.error(err));;
+  ).catch(err => console.error(err));
