da80e3bd31c52d3570725e66d4515c45b35ab1f1
Michael Stypa <michael@stypa.info>
2016-02-03 19:37:37 +0100

    lendings

--- _attachments/src/verleihfix/lendings.ts
+++ _attachments/src/verleihfix/lendings.ts
@@ -3,6 +3,11 @@ import {
   View
 } from 'angular2/core';
 import {
+  Http,
+  Headers,
+  HTTP_PROVIDERS
+} from 'angular2/http';
+import {
   ROUTER_PROVIDERS,
   ROUTER_DIRECTIVES,
   RouterOutlet,
@@ -13,11 +18,22 @@ import {
 } from 'angular2/router';
 
 @Component({
-  selector: 'lendings'
-})
-@View({
-  template: '<h2>{{ "LENDINGS" }}</h2>'
+selector: 'lendings',
+templateUrl: 'lendings.html'
 })
 export class Lendings {
+  items: any;
+  http: any;
 
+  constructor(http:Http) {
+    this.http = http;
+    this.http.get('/verleihfix/_design/verleihfix/_view/lendings')
+      .map(res => res.json().rows.map(res => res.value))
+      .subscribe(res => console.log(res));
+      //.subscribe(res => this.items = res);
+  }
+
+  delete(item:any) {
+    console.log(item);
+  }
 }
